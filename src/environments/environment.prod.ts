6bf61cef3bd7f96823ebcb2e9de26aad2433db5f
Matt Abbas <matthew.d.abbas@gmail.com>
2021-02-28 20:05:10 -0600

    Use https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authTokenServiceUrl: "http://api.mabalab.com/User?secret=",
+  authTokenServiceUrl: "https://api.mabalab.com/User?secret=",
   authTokenSecret: "hYvEa65R^R&48kp&9u*bUUwAw^%MpKcMt",
-  textAnalysisServiceUrl: "http://api.mabalab.com/ExternalSiteContent"
+  textAnalysisServiceUrl: "https://api.mabalab.com/ExternalSiteContent"
 };
